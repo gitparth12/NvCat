e1345a2b2c9ea426061f65af61f381878287d78c
Parth Bhargava <pbha6036@uni.sydney.edu.au>
2023-04-26 23:28:50 +1000

    trivial

--- lua/gitparth12/set.lua
+++ lua/gitparth12/set.lua
@@ -38,3 +38,6 @@ vim.o.updatetime = 250
 vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 
 vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]])
+
+-- clipboard
+vim.opt.clipboard = "unnamed"
