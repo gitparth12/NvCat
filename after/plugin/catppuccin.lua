e84dd3f21e8c51a4fd675aee951f2b86e3057f19
Parth Bhargava <parthbhargava1209@gmail.com>
2023-11-23 22:40:40 +1100

    Change term_colors in catppuccin

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -7,7 +7,7 @@ require("catppuccin").setup({
 	},
 	transparent_background = false,
 	show_end_of_buffer = true, -- show the '~' characters after the end of buffers
-	term_colors = false,
+	term_colors = true,
 	dim_inactive = {
 		enabled = false,
 		shade = "dark",
