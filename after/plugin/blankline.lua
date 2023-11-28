67b3f260f4bb7f5a4141d17443eadb70f3d4deed
Parth Bhargava <parthbhargava1209@gmail.com>
2023-11-28 17:00:49 +1100

    Update blankline settings

--- after/plugin/blankline.lua
+++ after/plugin/blankline.lua
@@ -1,7 +1,27 @@
 vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
 
-require("indent_blankline").setup({
-    show_end_of_line = true,
-    show_current_context = true,
+-- require("indent_blankline").setup({
+--     show_end_of_line = true,
+--     show_current_context = true,
+-- })
+require("ibl").setup({
+	indent = {
+		char = "|",
+		-- highlight = { "Function", "Label" },
+		smart_indent_cap = true,
+		priority = 2,
+	},
+	scope = {
+		enabled = true,
+		show_start = true,
+		show_end = true,
+		injected_languages = true,
+		highlight = { "Function", "Label" },
+		priority = 500,
+	},
+	whitespace = {
+		highlight = { "Function", "Label" },
+		remove_blankline_trail = true,
+	},
 })
