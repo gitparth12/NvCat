059ed1c9cd0d19b73448e583a6a76f818a44de8a
Parth Bhargava <parthbhargava1209@gmail.com>
2023-11-28 17:55:13 +1100

    Fix blankline config

--- after/plugin/blankline.lua
+++ after/plugin/blankline.lua
@@ -1,27 +1,37 @@
-vim.opt.list = true
-vim.opt.listchars:append("eol:↴")
+local highlight = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
+}
 
--- require("indent_blankline").setup({
---     show_end_of_line = true,
---     show_current_context = true,
--- })
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
 require("ibl").setup({
 	indent = {
 		char = "|",
-		-- highlight = { "Function", "Label" },
+		tab_char = { "|" },
 		smart_indent_cap = true,
 		priority = 2,
 	},
 	scope = {
-		enabled = true,
+		char = "|",
 		show_start = true,
-		show_end = true,
-		injected_languages = true,
-		highlight = { "Function", "Label" },
-		priority = 500,
-	},
-	whitespace = {
-		highlight = { "Function", "Label" },
-		remove_blankline_trail = true,
+		show_end = false,
+		-- highlight = highlight,
 	},
 })
