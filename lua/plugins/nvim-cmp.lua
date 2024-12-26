f67cd41414edac9ea41d3a6e16898f84cbabe08f
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2024-12-26 23:54:00 +0530

    Remove old lsp config, add new nvim-cmp config

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,50 @@
+return {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-buffer", -- source for text in buffer
+        "hrsh7th/cmp-path",  -- source for file in system paths
+        "L3MON4D3/LuaSnip", -- snippet engine
+        "saadparwaiz1/cmp_luasnip", -- for autocompletion
+        "rafamadriz/friendly-snippets", -- useful snippets
+    },
+    config = function()
+        local cmp = require("cmp")
+
+        local luasnip = require("luasnip")
+
+        -- loads vscode style snippets from installed plugins (eg. friendly-snippets)
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+            completion = {
+                completeopt = "menu,menuone,preview,noselect",
+            },
+            snippet = { -- configure how nvim-cmp interacts with snippet engine
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_jump),
+                ["<C-n>"] = cmp.mapping.select_next_item(cmp_jump),
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-y>"] = cmp.mapping.confirm({ select = false }),
+                ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+                ["<C-e>"] = cmp.mapping.abort(), -- show completion window
+            }),
+            sources = cmp.config.sources({
+                { name = "luasnip" }, -- snippets
+                { name = "buffer" }, -- text within current buffer
+                { name = "path" }, -- file system paths
+            }),
+        })
+    end,
+}
