a0b51e933c3a7411dddeabb3b1e3ac605dd85d21
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2024-12-27 00:32:31 +0530

    Add mason config

--- lua/parth/plugins/lsp/mason.lua
+++ lua/parth/plugins/lsp/mason.lua
@@ -3,4 +3,33 @@ return {
     dependencies = {
         "williamboman/mason-lspconfig.nvim",
     },
+    config = function()
+        local mason = require("mason")
+
+        local mason_lspconfig = require("mason-lspconfig")
+
+        -- enable mason and configure icons
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        })
+
+        mason_lspconfig.setup({
+            -- list of servers for mason to install
+            ensure_installed = {
+                "html",
+                "cssls",
+                "tailwindcss",
+                "lua_ls",
+                "pyright"
+            },
+            -- auto-install configured servers (with lspconfig)
+            automatic_installation = true, -- not the same as ensure_installed
+        })
+    end,
 }
