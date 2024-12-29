f92110e1854a4bf37ee21970e444803bbf579ee0
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2024-12-30 00:14:59 +0530

    Add linting

--- lua/parth/plugins/lsp/mason.lua
+++ lua/parth/plugins/lsp/mason.lua
@@ -1,35 +1,49 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        local mason = require("mason")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    local mason = require("mason")
 
-        local mason_lspconfig = require("mason-lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
 
-        -- enable mason and configure icons
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        })
+    local mason_tool_installer = require("mason-tool-installer")
 
-        mason_lspconfig.setup({
-            -- list of servers for mason to install
-            ensure_installed = {
-                "html",
-                "cssls",
-                "tailwindcss",
-                "lua_ls",
-                "pyright"
-            },
-            -- auto-install configured servers (with lspconfig)
-            automatic_installation = true, -- not the same as ensure_installed
-        })
-    end,
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        "html",
+        "cssls",
+        "tailwindcss",
+        "lua_ls",
+        "pyright",
+      },
+      -- auto-install configured servers (with lspconfig)
+      automatic_installation = true, -- not the same as ensure_installed
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint", -- python linter
+        "eslint_d", -- eslint linter
+      },
+    })
+  end,
 }
