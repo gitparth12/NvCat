11fb32c7e7103add1ba9761892e83f0684de02de
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-01-13 18:43:00 +1100

    Add python pynvim venv creation automation in parth.core in python.lua

---
+++ lua/parth/core/python.lua
@@ -0,0 +1,27 @@
+local python_version = "python3.12"
+
+local pynvim_env = vim.fn.stdpath("data") .. "/pynvim_env"
+if not vim.loop.fs_stat(pynvim_env) then
+  vim.fn.system({
+    "python",
+    "-m",
+    "venv",
+    pynvim_env,
+  })
+end
+
+local pynvim_python = pynvim_env .. "/bin/python"
+
+local pynvim_lib = pynvim_env .. "/lib/" .. python_version .. "/site-packages/pynvim"
+if not vim.loop.fs_stat(pynvim_lib) then
+  vim.fn.system({
+    pynvim_python,
+    "-m",
+    "pip",
+    "install",
+    "pynvim",
+  })
+end
+
+vim.g.python_host_prog = pynvim_python
+vim.g.python3_host_prog = pynvim_python
