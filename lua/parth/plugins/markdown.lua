e30d83143bbe4c9dc8f61747958b2ab2786e66d5
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-01-14 15:31:11 +1100

    Update render-markdown to resolve issues, comment out noice.nvim, and install venv-selector

--- lua/parth/plugins/markdown.lua
+++ lua/parth/plugins/markdown.lua
@@ -1,14 +1,14 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    opts = {},
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    config = function()
-        local map = require('parth.helpers.keys').map
-        local render = require('render-markdown')
-        map('n', '<leader>m', render.toggle)
-        -- map('n', '<leader>e', render.expand, {})
-        -- map('n', '<leader>c', render.contract, {})
-    end
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {},
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  config = function()
+    local map = require("parth.helpers.keys").map
+    local render = require("render-markdown")
+    map("n", "<leader>m", render.toggle)
+    -- map('n', '<leader>e', render.expand, {})
+    -- map('n', '<leader>c', render.contract, {})
+  end,
 }
