69ba9038e31adf15e926389b9e15c0ba47192de9
Parth Bhargava <41479054+gitparth12@users.noreply.github.com>
2025-01-19 16:24:19 +1100

    Replace hardcoded python version with better solution

--- lua/parth/core/python.lua
+++ lua/parth/core/python.lua
@@ -1,4 +1,5 @@
-local python_version = "python3.12"
+-- local python_version = "python3.12"
+local python_version = vim.fn.system("python3 --version"):match("%d+%.%d+")
 
 local pynvim_env = vim.fn.stdpath("data") .. "/pynvim_env"
 if not vim.loop.fs_stat(pynvim_env) then
@@ -25,3 +26,5 @@ end
 
 vim.g.python_host_prog = pynvim_python
 vim.g.python3_host_prog = pynvim_python
+-- vim.g.python_host_prog = "/Users/parthbhargava/.pyenv/versions/diversity/bin/python"
+-- vim.g.python3_host_prog = "/Users/parthbhargava/.pyenv/versions/diversity/bin/python"
